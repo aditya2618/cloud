51d98cd21dbe54ee12c92ad55d39c2c25f3d7e5c
Cloud Backend <bmanpart3@gmail.com>
2026-01-10 21:12:16 +0530

    Add GCP IP to ALLOWED_HOSTS

--- smarthome_cloud/settings.py
+++ smarthome_cloud/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-CHANGE-THIS-IN-PRODUCTION'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,35.209.239.164').split(',')
 
 # Allow URLs without trailing slashes (important for mobile app POST requests)
 APPEND_SLASH = False
